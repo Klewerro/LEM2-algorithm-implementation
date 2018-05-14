5519dc1df31baadce07e2e483c420e79f99569fd
Klewerro <pol.szym95@gmail.com>
2018-05-14 21:34:51 +0200

    lower approximation
    Finding for sure true rows.

---
+++ site/scripts.js
@@ -0,0 +1,55 @@
+var array = [["Wysoka", "Tak", "Nie"],
+    ["Bardzo wyskoka", "Tak", "Tak"],
+    ["Normalna", "Tak", "Nie"],
+    ["Wysoka", "Nie", "Nie"],
+    ["Wysoka", "Tak", "Tak"],
+    ["Normalna", "Tak", "Nie"],
+    ["Bardzo wysoka", "Nie", "Nie"],
+    ["Wysoka", "Nie", "Nie"]
+];
+
+var decisions = ["Tak", "Tak", "Tak", "Tak", "Nie", "Nie", "Nie", "Nie"];
+
+var lowApprox = lowerApproximation(array, "Tak");
+
+function lowerApproximation(array, decision) {
+    var pLowArray = [];
+    var innerTrueFalseCounter= 0;
+
+    for (var i=0; i < array.length; i++) {
+        if (decision == decisions[i]) {
+            for (var j=0; j < array.length; j++) {            
+                if (compareTwoRows(array[i], array[j]) && decisions[i] != decisions[j] && i != j) {
+                    break;
+                } else {
+                    innerTrueFalseCounter++;
+                }
+            }
+            if (innerTrueFalseCounter == array.length) {
+                pLowArray[i] = true;
+            } else {
+                pLowArray[i] = false;
+            }
+            innerTrueFalseCounter = 0;
+        } else {
+            pLowArray[i] = false;
+        }
+        
+    }
+
+    return pLowArray;
+}
+
+
+function compareTwoRows(row1 = [], row2 = []) {
+
+    for(var k = 0; k < row1.length; k++) {
+        if (row1[k] != row2[k]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
